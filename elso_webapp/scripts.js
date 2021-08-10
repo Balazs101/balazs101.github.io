3812d6d070daaf02ef11197434ef223ca30df784
Balazs101 <gedei.balazs@gmail.com>
2021-08-10 15:14:21 +0200

    második napi feladatok

---
+++ elso_webapp/scripts.js
@@ -0,0 +1,44 @@
+let car = {
+    photo: './images/car.jpg',
+    title: 'KEP NEVE',
+    description: 'KEP LEIRASA'
+  };
+
+  let depot = {
+    photo: './images/depot.jpg',
+    title: 'KEP NEVE',
+    description: 'KEP LEIRASA'
+  };
+
+  let house = {
+    photo: './images/house.jpg',
+    title: 'KEP NEVE',
+    description: 'KEP LEIRASA'
+  };
+
+  let sea = {
+    photo: './images/sea.jpg',
+    title: 'KEP NEVE',
+    description: 'KEP LEIRASA'
+  };
+
+  let stone = {
+    photo: './images/stone.jpg',
+    title: 'KEP NEVE',
+    description: 'KEP LEIRASA'
+  };
+
+  let street = {
+    photo: './images/street.jpg',
+    title: 'KEP NEVE',
+    description: 'KEP LEIRASA'
+  }; 
+
+let currentPhoto = 0;
+let imagesData = []
+
+
+
+
+
+  $('#photo').attr('src', car.photo);
